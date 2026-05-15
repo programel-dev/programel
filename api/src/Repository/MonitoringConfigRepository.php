88e0c9d8dd1d5f3cb7e62a37cda8183cf30f9244
programel <programel.dev@gmail.com>
2026-05-15 23:57:39 +0200

    feat(monitoring): add MonitoringConfig entity, repository, migration

---
+++ api/src/Repository/MonitoringConfigRepository.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Repository;
+
+use App\Entity\MonitoringConfig;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+
+/**
+ * @extends ServiceEntityRepository<MonitoringConfig>
+ */
+class MonitoringConfigRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, MonitoringConfig::class);
+    }
+
+    public function isEnabled(): bool
+    {
+        return $this->find(1)?->isEnabled() ?? true;
+    }
+
+    public function getOrCreate(): MonitoringConfig
+    {
+        return $this->find(1) ?? new MonitoringConfig();
+    }
+}
